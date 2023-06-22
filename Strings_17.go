c017d0c978cf5a87db96c9645563f95838a1462a
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-22 19:04:34 -0300

    Strings_17.go

---
+++ Strings_17.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var nomes string
+	fmt.Print("Digite uma string: ")
+	fmt.Scanln(&nomes)
+	LU := getUniqueLetters(nomes)
+	fmt.Println("Letras únicas na string:", LU)
+}
+func getUniqueLetters(input string) string {
+	unicas := ""
+	javistas := make(map[rune]int)
+	for _, letras := range input {
+		javistas[letras]++
+	}
+	for _, char := range input {
+		if javistas[char] == 1 {
+			unicas += string(char)
+		}
+	}
+	return unicas
+}
