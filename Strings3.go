4ed81558942cdb7bd58ae6a8fa33cf380c056734
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-22 18:55:55 -0300

    Strings3.go

---
+++ Strings3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+func main() {
+	var nome string
+	var Al, Nl string
+	fmt.Print("Digite uma string: ")
+	fmt.Scanln(&nome)
+	fmt.Print("Digite o caractere a ser substituído: ")
+	fmt.Scanln(&Al)
+	fmt.Print("Digite o novo caractere: ")
+	fmt.Scanln(&Nl)
+	NovaString := strings.ReplaceAll(nome, Al, Nl)
+	fmt.Println("String atualizada:", NovaString)
+}
