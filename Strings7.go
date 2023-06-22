a497634403a51840804aaee0b2eae9465ed7cbb5
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-22 18:57:59 -0300

    Strings7.go

---
+++ Strings7.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+func main() {
+	var texto string
+	fmt.Print("Digite uma string: ")
+	fmt.Scanln(&texto)
+	num := false
+	for _, Letra := range texto {
+		if unicode.IsDigit(Letra) {
+			num = true
+			break
+		}
+	}
+	if num {
+		fmt.Println("A string contém pelo menos um número.")
+	} else {
+		fmt.Println("A string não contém números.")
+	}
+}
