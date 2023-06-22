91b5bf84f6cd8008bfbaba530b4d1742a001ada1
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-22 18:56:12 -0300

    Strings4.go

---
+++ Strings4.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var string1, string2 string
+	fmt.Print("Digite a primeira string: ")
+	fmt.Scanln(&string1)
+	fmt.Print("Digite a segunda string: ")
+	fmt.Scanln(&string2)
+	if string1 == string2 {
+		fmt.Println("As strings são iguais.")
+	} else {
+		fmt.Println("As strings são diferentes.")
+	}
+}
