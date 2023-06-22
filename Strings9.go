62a63afb1478bcae543c6676ef2daf1d77b0b36f
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-22 18:58:43 -0300

    Strings9.go

---
+++ Strings9.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+func main() {
+	var nome, Al, Nl string
+	fmt.Print("Digite uma string: ")
+	fmt.Scanln(&nome)
+	fmt.Print("Digite o caractere a ser substituído: ")
+	fmt.Scanln(&Al)
+	fmt.Print("Digite o novo caractere: ")
+	fmt.Scanln(&Nl)
+	NS := strings.ReplaceAll(nome, Al, Nl)
+	fmt.Println("String atualizada:", NS)
+}
