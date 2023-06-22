88058c5e9fe7b99eb7c2ba6f853a888c007733d2
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-22 18:59:51 -0300

    Strings_11.go

---
+++ Strings_11.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+func main() {
+	var nomes string
+	fmt.Print("Digite uma string: ")
+	fmt.Scanln(&nomes)
+	vogaisremovidas := vogaisremovidas(nomes)
+	fmt.Println("String sem vogais:", vogaisremovidas)
+}
+func vogaisremovidas(input string) string {
+	vogais := "aeiouAEIOU"
+	resultado := ""
+	for _, letra := range input {
+		if !strings.ContainsRune(vogais, letra) {
+			resultado += string(letra)
+		}
+	}
+	return resultado
+}
